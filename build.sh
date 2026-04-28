9166686ee769bf5d2915a0d465d34cc7502b2241
Mohammed Ajaz <ajazz.md@gmail.com>
2026-04-28 10:43:48 +0530

    remove unnecessary copy step

--- build.sh
+++ build.sh
@@ -3,17 +3,6 @@ set -e
 
 pip install -r requirements.txt
 
-# go to frontend and build
 cd frontend
 npm install
 npm run build
-
-# go back to root
-cd ..
-
-# clean static folder
-rm -rf backend/static
-mkdir -p backend/static
-
-# copy correctly (FIXED PATH)
-cp -r frontend/dist/* backend/static/

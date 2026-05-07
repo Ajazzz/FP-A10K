ae164a1cae20cdd148666cd30e8158b62496cbd6
Mohammed Ajaz <ajazz.md@gmail.com>
2026-05-07 18:35:35 +0530

    Fix Render build script

--- build.sh
+++ build.sh
@@ -4,18 +4,13 @@ set -o errexit
 
 echo "Installing frontend dependencies..."
 cd frontend
+
 npm install
 
 echo "Building frontend..."
 npm run build
 
 cd ..
-
-echo "Copying frontend build to backend/static..."
-rm -rf backend/static
-mkdir -p backend/static
-
-cp -r frontend/dist/* backend/static/
 
 echo "Installing backend dependencies..."
 pip install -r requirements.txt
